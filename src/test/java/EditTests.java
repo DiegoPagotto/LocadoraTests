fdf2fe7f63dd26b4916f2fe6900e48a06c49a1d6
Ingridnery <neryingrid16@gmail.com>
2023-05-25 15:28:00 -0300

    test: add test case to edit car and verify table update

--- src/test/java/EditTests.java
+++ src/test/java/EditTests.java
@@ -5,8 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
+import java.time.Duration;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -57,6 +61,22 @@ public class EditTests {
         buttonSave.click();
         assertTrue(driver.findElement(By.id("editModal")).isEnabled());
     }
+    @Test
+    @DisplayName("Should edit car and update table")
+    void shouldEditCarAndUpdateTable(){
+        WebElement buttonEdit= driver.findElement(By.cssSelector("table#carrosTable .edit-button"));
+        buttonEdit.click();
+        WebElement inputColor = driver.findElement(By.id("cor"));
+        inputColor.clear();
+        inputColor.sendKeys("Amarelo");
+        WebElement buttonSave = driver.findElement(By.cssSelector("#editForm input[type='submit']"));
+        buttonSave.click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.stalenessOf(buttonSave));
+        WebElement column = driver.findElement(By.xpath("//*[@id=\"carrosTable\"]/tbody/tr[2]/td[5]"));
+        assertThat(column.getText()).isEqualTo("Amarelo");
+    }
+
 
 
 }
