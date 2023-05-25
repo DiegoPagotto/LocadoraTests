0ebcac3bebfac8b492bd72f185a279f93c8989f8
Ingridnery <neryingrid16@gmail.com>
2023-05-25 13:42:03 -0300

    feat: adds WebDriver configuration in EditTests

--- src/test/java/EditTests.java
+++ src/test/java/EditTests.java
@@ -1,18 +1,23 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class EditTests {
-    @Test
-    @DisplayName("Should open and close chrome browser using Manager")
-        //Esse caso de teste é só um exemplo
-    void shouldOpenAndCloseChromeBrowserUsingManager() throws InterruptedException {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://scl.ifsp.edu.br");
-        Thread.sleep(5000);
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get("C:\\SeleniumTestNew\\frontend\\home\\index.html\n");
+    }
+    @AfterEach
+    public void tearDown() {
         driver.quit();
     }
 }
