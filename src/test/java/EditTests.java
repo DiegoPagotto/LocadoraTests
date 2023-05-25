93bd0ef9dedf381c76cceea62f19ae406d6d0146
Ingridnery <neryingrid16@gmail.com>
2023-05-25 15:53:22 -0300

    test: add test case to closed edit modal when click on close

--- src/test/java/EditTests.java
+++ src/test/java/EditTests.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import java.time.Duration;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -75,6 +77,19 @@ public class EditTests {
         wait.until(ExpectedConditions.stalenessOf(buttonSave));
         WebElement column = driver.findElement(By.xpath("//*[@id=\"carrosTable\"]/tbody/tr[2]/td[5]"));
         assertThat(column.getText()).isEqualTo("Amarelo");
+    }
+    @Test
+    @DisplayName("Should closed edit modal when click on close button")
+    void shouldClosedEditModalWhenClickOnCloseButton(){
+        WebElement buttonEdit= driver.findElement(By.cssSelector("table#carrosTable .edit-button"));
+        buttonEdit.click();
+        new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofMillis(700))
+                .ignoring(NoSuchElementException.class)
+                .until(ExpectedConditions.elementToBeClickable(By.className("close")))
+                .click();
+        assertThat(driver.findElement(By.id("editModal")).isDisplayed()).isFalse();
     }
 
 
