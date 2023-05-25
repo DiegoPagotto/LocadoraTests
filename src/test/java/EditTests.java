6a235957aa5a74ead8c78f2b05bf0bbf8ea947b4
DiegoPagotto <diegodeabreupagotto@gmail.com>
2023-05-25 10:55:31 -0300

    feat: creates class for editing cars tests

---
+++ src/test/java/EditTests.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class EditTests {
+    @Test
+    @DisplayName("Should open and close chrome browser using Manager")
+        //Esse caso de teste é só um exemplo
+    void shouldOpenAndCloseChromeBrowserUsingManager() throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://scl.ifsp.edu.br");
+        Thread.sleep(5000);
+        driver.quit();
+    }
+}
