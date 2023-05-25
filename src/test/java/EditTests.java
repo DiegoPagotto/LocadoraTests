5fca216eaea543bdfd0cdcaa3ca3c071b9c8a2da
Ingridnery <neryingrid16@gmail.com>
2023-05-25 14:06:27 -0300

    test: adds test case to open the edit modal

--- src/test/java/EditTests.java
+++ src/test/java/EditTests.java
@@ -1,8 +1,13 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class EditTests {
     private WebDriver driver;
@@ -20,4 +25,12 @@ public class EditTests {
     public void tearDown() {
         driver.quit();
     }
+    @Test
+    @DisplayName("Should open edit modal")
+    void shouldOpenEditModal(){
+        WebElement buttonEdit= driver.findElement(By.cssSelector("table#carrosTable .edit-button"));
+        buttonEdit.click();
+        assertTrue(driver.findElement(By.id("editModal")).isEnabled());
+    }
+
 }
