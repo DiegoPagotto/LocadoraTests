1120dc19f209ef5d189cd049740745aab0e2291b
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-06-01 18:30:11 -0300

    test: Add setUp and tearDown to DeleteTests

--- src/test/java/DeleteTests.java
+++ src/test/java/DeleteTests.java
@@ -1,16 +1,37 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.assertj.core.api.Assertions.assertThat;
 public class DeleteTests {
+    private WebDriver driver;
+
+
+    @BeforeEach
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        driver = new ChromeDriver(options);
+        driver.get("C:\\SeleniumTestNew\\frontend\\home\\index.html\n");
+    }
+    @AfterEach
+    public void tearDown() {
+        driver.quit();
+    }
 
     @Test
     @DisplayName("Should open and close chrome browser using Manager")
