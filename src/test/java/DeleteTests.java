71ca5c7b5d35890c11b360b3593fffbad86e9b0a
GuilhermeGM4 <91355069+GuilhermeGM4@users.noreply.github.com>
2023-06-06 16:45:28 -0300

    refactor: Small changes in logic in 'should delete all entries in the table' in the first conditional to stop using size comparator, in 'for' to decrease from total to 0 and assertThat to check if is empty

--- src/test/java/DeleteTests.java
+++ src/test/java/DeleteTests.java
@@ -82,18 +82,17 @@ public class DeleteTests {
 
     @Test
     @DisplayName("Should delete all entries in the table")
-    void shouldDeleteAll(){
+    void shouldDeleteAll() throws InterruptedException {
         final List<WebElement> delBtns = driver.findElements(By.className("delete-button"));
-        if(delBtns.size() == 0){
+        if(delBtns.isEmpty()){
             assertThat("Empty table").isEqualTo("Table with content");
         }
-        int tableLen = delBtns.size() + 1;
-        for(int i = 2; i < tableLen; i++){
+        for(int i = delBtns.size(); i > 0; i--){
             driver.findElement(By.className("delete-button")).click();
             driver.navigate().refresh();
         }
         final List<WebElement> tableLenNew = driver.findElements(By.className("delete-button"));
-        assertThat(tableLenNew.size()).isLessThan(delBtns.size());
+        assertThat(tableLenNew).isEmpty();
     }
 
     @Test
