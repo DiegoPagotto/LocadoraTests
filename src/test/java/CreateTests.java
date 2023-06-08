f9e1240410d47f17ab8c181c2b46d2f39a716082
DiegoPagotto <diegodeabreupagotto@gmail.com>
2023-06-08 12:54:31 -0300

    test (create car): adds test for checking if spaces counts as valid attributes for cars

--- src/test/java/CreateTests.java
+++ src/test/java/CreateTests.java
@@ -8,6 +8,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -57,6 +61,38 @@ public class CreateTests {
         }
 
     }
+    
+    @Test
+    @DisplayName("Should not be able to register a car with space as attribute")
+    void shouldNotBeAbleToRegisterACarWithSpaceAsAttribute(){
+        int numberOfCars = getNumbersOfCars();
+        goToRegisterPage();
+        WebElement brandInput = driver.findElement(By.id("marca"));
+        brandInput.sendKeys(" ");
+
+        WebElement modelInput = driver.findElement(By.id("modelo"));
+        modelInput.sendKeys(" ");
+
+        WebElement yearInput = driver.findElement(By.id("ano"));
+        yearInput.sendKeys("0");
+
+        WebElement colorInput = driver.findElement(By.id("cor"));
+        colorInput.sendKeys(" ");
+
+        WebElement plateInput = driver.findElement(By.id("placa"));
+        plateInput.sendKeys(" ");
+
+        WebElement submitButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        submitButton.click();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("carrosTable")));
+
+
+        // nenhum carro novo pode ter sido adicionado
+        assertThat(getNumbersOfCars()).isEqualTo(numberOfCars);
+
+    }
 
     private void goToHomePage() {
         WebElement buttonHome = driver.findElement(By.xpath("/html/body/div/nav/ul/li[1]"));
@@ -66,6 +102,12 @@ public class CreateTests {
     private void goToRegisterPage() {
         WebElement buttonCreate = driver.findElement(By.xpath("/html/body/div/nav/ul/li[2]"));
         buttonCreate.click();
+    }
+
+    private int getNumbersOfCars(){
+        WebElement table = driver.findElement(By.id("carrosTable"));
+        java.util.List<WebElement> rows = table.findElements(By.tagName("tr"));
+        return rows.size();
     }
 
 
